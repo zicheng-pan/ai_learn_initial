8e0bb630a768e618e8a7dc20b6e132231f3a1ea1
zicheng <1094275633@qq.com>
2025-06-19 21:58:52 +0800

    添加线性回归的负数情况

--- 线性回归/01.py
+++ 线性回归/01.py
@@ -4,21 +4,25 @@ from matplotlib import pyplot as plt
 
 # generate Data
 def get_beans(counts):
-    xs = np.random.random(counts)
+    xs = np.sort(np.random.random(counts))
     xs = np.sort(xs)
-    ys = np.array([10 * x + np.random.rand() / 10 for x in xs])
+    ys = np.array([1.5 * x + np.random.rand() / 10 for x in xs])
+
+    # 这里假设数据有负数
+    xs = xs * -1
+    ys = ys * -1
     return xs, ys
 
-
-x, y = get_beans(100)
+count = 100
+x, y = get_beans(count)
 
 
 # 预测值y_pre = w*x+b 这里只预测w
 def pre_Forward(x, y, w, alpha):
-    for item in x:
-        y_pre = w * item
-        loss = y - y_pre
-        w = w + loss * alpha
+    for index in range(count):
+        y_pre = w * x[index]        #前向传播
+        loss = y[index] - y_pre
+        w = w + loss * alpha * x[index]   #反向传播  这里采用了x这个值的符号来学习，x为负，误差为负就增加
         draw(x, y, calc_pre_w(x, w))
 
 
@@ -29,7 +33,7 @@ def calc_pre_w(x, w):
 
 def draw(x, y, y_pre, method=plt.scatter):
     # 画线
-    method = plt.plot
+    method = plt.plot # 正常情况下是拟合，但是如果直接用这个plot这个就是过拟合
     plt.clf()
 
     plt.title("Size-Toxicity Function", fontsize=12)
